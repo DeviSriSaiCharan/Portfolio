b8aa2e8c4e653191bc64c5f562b871ee75275711
DeviSriSaiCharan <devisrisaicharan@gmail.com>
2026-06-17 17:11:00 +0530

    feat: add conditional rendering for Google Analytics in non-production environments

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -1,6 +1,11 @@
 import Script from "next/script";
 
 export function GoogleAnalytics() {
+
+    if (process.env.NODE_ENV !== "production") {
+        return null;
+    }
+
   return (
     <>
       <Script
